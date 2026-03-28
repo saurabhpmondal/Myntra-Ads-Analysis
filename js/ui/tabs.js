55a7efb228b9f42bbb0d9b57fc3d0a0411fb4368
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-28 23:50:03 +0530

    Update tabs.js

--- js/ui/tabs.js
+++ js/ui/tabs.js
@@ -1,10 +1,6 @@
-export function initTabs() {
-  const tabs = document.querySelectorAll(".tab");
-
-  tabs.forEach(tab => {
-    tab.onclick = () => {
-      tabs.forEach(t => t.classList.remove("active"));
-      tab.classList.add("active");
-    };
-  });
-}
+document.querySelectorAll(".tab").forEach(tab=>{
+  tab.onclick = ()=>{
+    document.querySelectorAll(".tab").forEach(t=>t.classList.remove("active"));
+    tab.classList.add("active");
+  }
+});
