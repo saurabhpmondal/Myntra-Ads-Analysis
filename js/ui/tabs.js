ef46fcdff93328b48705b470e03820ec4a4411b8
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-28 23:37:26 +0530

    Update tabs.js

--- js/ui/tabs.js
+++ js/ui/tabs.js
@@ -1,13 +1,10 @@
-function initTabs() {
+export function initTabs() {
   const tabs = document.querySelectorAll(".tab");
-  const content = document.getElementById("contentArea");
 
   tabs.forEach(tab => {
     tab.onclick = () => {
       tabs.forEach(t => t.classList.remove("active"));
       tab.classList.add("active");
-
-      content.innerHTML = `<div>${tab.dataset.tab} view</div>`;
     };
   });
 }
