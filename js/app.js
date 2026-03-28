ddabaafbc3dd5ddc363a3784fd24b609f2a42f60
saurabhpmondal <saurabhpmondal@gmail.com>
2026-03-28 23:36:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,9 @@
-import { loadAllData } from "./core/dataLoader.js";
-import { normalizeCDR, normalizeGeneric } from "./core/dataParser.js";
-import { setData } from "./core/dataStore.js";
-import { renderKPIs } from "./ui/kpiCards.js";
-import { renderChart } from "./ui/chart.js";
+import { initFilter } from "./ui/filter.js";
+import { initTabs } from "./ui/tabs.js";
+import { initDropdown } from "./ui/dropdown.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
-
-  initDropdowns();
+document.addEventListener("DOMContentLoaded", () => {
+  initFilter();
   initTabs();
-
-  const raw = await loadAllData();
-
-  const parsed = {
-    CDR: normalizeCDR(raw.CDR),
-    CPR: normalizeGeneric(raw.CPR),
-    PPR: normalizeGeneric(raw.PPR),
-    VIEWS: normalizeGeneric(raw.VIEWS)
-  };
-
-  setData(parsed);
-
-  renderKPIs();
-
-  // 🔥 Render chart default
-  renderChart("Spend", "Revenue");
+  initDropdown();
 });
